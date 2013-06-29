94477c8b4124ff14319c0f0ae845aa939d9e6dab
gonjay <assyer@gmail.com>
2013-06-30 00:26:01 +0800

    优化500页面，即找不到书的返回页面；优化搜索结果的显示内容

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 
 class Book < ActiveRecord::Base
   attr_accessible :name
-  attr_accessor :index_number, :status, :img_src, :id_code
+  attr_accessor :index_number, :status, :img_src, :id_code, :auther, :publisher
 
   def search_index(params)
     @url = 'http://202.114.181.3:8080/opac/openlink.php?' +
@@ -30,6 +30,16 @@ class Book < ActiveRecord::Base
       @book.name = item.css('h3')[0].css('a')[0].content
       @book.index_number = item.css('h3')[0].css('span')[0].content + item.css('h3')[0].content.split("#{@book.name}")[1]
       @book.status = item.css('p')[0].css('span')[0].content
+      p_content = item.css('p')[0].content.split("#{@book.status}")[1]
+      if p_content.include?(".")
+        p = p_content.split(".")
+        @book.auther = p[0]
+        @book.publisher = p[1]
+      else
+        p = p_content.split("著")
+        @book.auther = p[0]
+        @book.publisher = p[1]
+      end
       @books << @book
      end
 
